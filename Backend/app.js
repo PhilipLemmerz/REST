32a9078094681a0bd1edfb2875315c99718dcdff
Philip Lemmerz <plemmerz@yahoo.de>
2022-06-10 16:28:42 +0200

    deployementConfig

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const adminRoutes = require('./router/admin-routes');
 const authRoutes = require('./router/auth-routes');
-const dbKey = require('./databse-key');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan =  require('morgan');
+const fs = require('fs');
+const path = require('path');
+
 
 const app = express();
 
@@ -19,6 +24,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Header');
   next();
 });
+
+const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: logStream}));
 
 app.use('/admin/', adminRoutes);
 app.use('/auth/', authRoutes);
@@ -36,7 +47,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-mongoose.connect(dbKey).then(() => {
+mongoose.connect(`${process.env.MONGO_DB_KEY}`).then(() => {
   const server = app.listen(8080);
   const socketIO = require('./socket').init(server);
   socketIO.on('connection', socket => {
--- Backend/databse-key.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'mongodb+srv://philipLemmerz:Penelope2202@cluster0.okhjbuk.mongodb.net/?retryWrites=true&w=majority'
